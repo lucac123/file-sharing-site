9838f2f115853df1636b44c13e1a248adaceeee5
lucac123 <lchackalackal@gmail.com>
2021-10-15 16:54:51 -0500

    Created HTML for Homepage

--- fileshare.php
+++ fileshare.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>FileShare</title>
-		<meta charset="utf-8"/>
-		<link rel="stylesheet" type="text/css" href="fileshare.css"/>
-	</head>
-	<body>
-		
-	</body>
+
+<head>
+	<title>FileShare</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="fileshare.css" />
+</head>
+
+<body>
+	<header>
+		<h1><span>FileShare</span> - Free Online File-Hosting Service</h1>
+	</header>
+	<nav>
+		<a href="fileshare.php">Home</a>
+		<a href="files.php">Files</a>
+		<?php
+session_start();
+if (isset($_SESSION['user']))
+	echo '<a href="logout.php">Logout</a>';
+else
+	echo '<a href="login.php">Login</a>';
+		?>
+	</nav>
+	<section>
+		<a href="files.php">Upload or View Files</a>
+	</section>
+
+</body>
+
 </html>
