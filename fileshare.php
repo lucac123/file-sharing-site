9da450ee9ff1cb05ef7f0be3be20723e541baab9
lucac123 <lchackalackal@gmail.com>
2021-10-15 22:27:16 -0500

    Updated fileshar.php nav links

--- fileshare.php
+++ fileshare.php
@@ -12,14 +12,14 @@
 		<h1><span>FileShare</span> - Free Online File-Hosting Service</h1>
 	</header>
 	<nav>
-		<a href="fileshare.php">Home</a>
+		<a href=".">Home</a>
 		<a href="files.php">Files</a>
 		<?php
 session_start();
 if (isset($_SESSION['user']))
 	echo '<a href="logout.php">Log Out</a>';
 else {
-	echo '<a href="login.php?signup">Sign Up</a>';
+	echo '<a href="login.php?signup">Sign Up</a>'."\n";
 	echo '<a href="login.php">Log In</a>';
 }
 		?>
